af09d64de39d2ddde161c18f60d0d0dd98cea492
AsbertMa <stayinalive@msn.cn>
2022-10-09 16:19:26 +0800

    use the layout

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,29 @@
 <template>
-  <Container />
+  <n-layout content-style="display: flex; flex-direction: column;">
+    <n-layout-header bordered>
+      <Navbar/>
+    </n-layout-header>
+    <n-layout style="height: inherit" has-sider>
+      <n-layout-sider bordered>
+        Handian Bridge
+      </n-layout-sider>
+      <n-layout-content bordered>
+        <Container/>
+      </n-layout-content>
+    </n-layout>
+  </n-layout>
+  <!-- <Navbar />
+  <div style="height: inherit">
+    <div style="display: flex; flex-direction: row; height: 100%;">
+      <div>side bar</div>
+      <Container />
+    </div>
+  </div> -->
 </template>
 <script setup lang="ts">
+import { NLayout, NLayoutHeader, NLayoutSider, NLayoutContent } from 'naive-ui'
 import Container from './components/Container.vue'
+import Navbar from './components/Navbar.vue'
 </script>
 
 <style>
@@ -12,5 +33,8 @@ body,
   margin: 0;
   height: 100vh;
   width: 100vw;
+  display: flex;
+  flex-direction: column;
+  flex: 1 1;
 }
 </style>
