34340bbc5c45feac9083b642337e83de08ee66dc
AsbertMa <stayinalive@msn.cn>
2022-12-16 16:21:54 +0800

    connex

--- src/svc/connex.ts
+++ src/svc/connex.ts
@@ -3,17 +3,19 @@ import { Node } from './storage'
 
 const connexList: Map<number, Connex> = new Map<number, Connex>()
 
-export default function getConnex (node: Node) {
-    if (connexList.get(node.id!)){
-        return connexList.get(node.id!)
+export default function getConnex(node: Node) {
+    let _connex = connexList.get(node.id!)
+    if (_connex) {
+        return _connex
+    } else {
+        _connex = new Connex({
+            node: node.url,
+            network: ['custom', 'solo'].includes(node.network)
+                ? JSON.parse(node.block_zero!) as Connex.Thor.Block
+                : node.network as 'main' | 'test'
+        })
+        connexList.set(node.id!, _connex)
+        return _connex
     }
 
-    const _connex = new Connex({
-        node: node.url,
-        network: node.network === 'custom' ? JSON.parse(node.block_zero!) as Connex.Thor.Block : node.network
-    })
-
-    connexList.set(node.id!, _connex)
-
-    return _connex
 }
